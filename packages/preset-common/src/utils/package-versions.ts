e5b23f3d56cb318348e3499ce4e315420296f55d
TraveWise <wise0shark@gmail.com>
2024-05-19 06:52:08 -0400

    sora

---
+++ packages/preset-common/src/utils/package-versions.ts
@@ -0,0 +1,34 @@
+export const packageVersion = {
+  '@coral-xyz': {
+    anchor: '^0.29.0',
+  },
+  '@tailwindcss': {
+    typography: '0.5.10',
+  },
+  '@solana': {
+    'spl-token': '0.4.1',
+    'wallet-adapter-base': '^0.9.23',
+    'wallet-adapter-react': '^0.15.35',
+    'wallet-adapter-react-ui': '^0.9.35',
+    'web3.js': '1.90.0',
+  },
+  '@swc-node': {
+    core: '1.12.0',
+    register: '1.8.0',
+  },
+  '@tabler': {
+    'icons-react': '2.47.0',
+  },
+  '@tanstack': {
+    'react-query': '5.24.1',
+    'react-query-next-experimental': '5.24.1',
+  },
+  'react-hot-toast': '2.4.1',
+  'crypto-browserify': '3.12.0',
+  'stream-browserify': '3.0.0',
+  bs58: '5.0.0',
+  buffer: '6.0.3',
+  daisyui: '4.7.2',
+  encoding: '0.1.13',
+  jotai: '2.6.5',
+}
